83237f595537c74a729e6edddd89652a164c3339
Rico Jasper <jasper.rico@gmail.com>
2024-08-23 23:21:36 +0200

    Add examples

---
+++ test/matcher/examples_test.rb
@@ -0,0 +1,118 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'matcher/testing'
+
+module Matcher
+  class ExamplesTest < ActiveSupport::TestCase
+    include Testing
+
+    test 'tree' do
+      matcher = Matcher.build do
+        declare :low, :high
+
+        refs[:node] = {
+          key: all(Integer, lo { (!low | (_ > low)) & (!high | (_ < high)) }),
+          left: setvar(high: ->(high:, parent:) { [parent[:key], high].compact.max }) ^
+            (of(nil) | refs[:node]),
+          right: setvar(low: ->(low:, parent:) { [parent[:key], low].compact.min }) ^
+            (of(nil) | refs[:node]),
+        }
+      end
+
+      tree = {
+        key: 5,
+        left: {
+          key: 3,
+          left: { key: 1, left: nil, right: nil },
+          right: { key: 4, left: nil, right: nil },
+        },
+        right: {
+          key: 7,
+          left: { key: 5, left: nil, right: nil }, # error: key equal to root key
+          right: { key: 10, left: nil, right: nil },
+        }
+      }
+
+      expected = 'expected (!low || actual > low) && (!high || actual < high) to be truthy for low = 5, actual = 5, high = 7'
+
+      assert_errors matcher.match(tree),
+        right: { left: { key: expected } }
+    end
+
+    test 'cyclic graph' do
+      matcher = Matcher.build do
+        refs[:vertex] = setvar(vertex: -> { _1 }) ^ {
+          name: String,
+          edges: each(refs[:edge, cyclic: true]),
+        }
+
+        refs[:edge] = {
+          weight: Integer,
+          destination: refs[:vertex, cyclic: true]
+        }
+
+        # graph
+        { vertices: each(refs[:vertex]) }
+      end
+
+      a = { name: 'a', edges: [] }
+      b = { name: 'b', edges: [] }
+      c = { name: 'c', edges: [] }
+      graph = { vertices: [a, b, c]}
+
+      a[:edges] << { weight: 1, destination: b }
+      b[:edges] << { weight: 2, destination: c }
+
+      assert_no_errors matcher.match(graph)
+
+      c[:edges] << { weight: 3, destination: a }
+
+      assert_no_errors matcher.match(graph)
+    end
+
+    test 'pipe' do
+      matcher = Matcher.build do
+        setvar(c: 1) ^ setvar(c: ->(c:) { c + 1 }) ^ {
+          value: _ == vars[:c],
+        }
+      end
+
+      assert_predicate matcher.match({ value: 2 }), :valid?
+    end
+
+    test 'map ^ each' do
+      matcher = Matcher.build do
+        map(_.length) ^ each(3)
+      end
+
+      assert_predicate matcher.match(%w[123 456]), :valid?
+
+      assert_errors matcher.match(%w[123 456 7890]),
+        2 => { Call.build { _1.length } => 'expected 3 but got 4' }
+    end
+
+    test 'expressions: constant receiver' do
+      matcher = Matcher.build do
+        expr(Math).sqrt(_) > 2
+      end
+
+      assert_equal 'Math.sqrt(_) > 2', matcher.inspect
+
+      assert_no_errors matcher.match(9)
+      assert_errors matcher.match(4),
+        'expected Math.sqrt(_) to be > 2 but got 2.0 for actual = 4'
+    end
+
+    test 'expressions: block receiver' do
+      matcher = Matcher.build do
+        expr_s { |_| [_, 10] }.sum >= 15
+      end
+
+      assert_equal 'expr_s { |_| [_, 10] }.sum >= 15', matcher.inspect
+      assert_no_errors matcher.match(10)
+      assert_errors matcher.match(2),
+        'expected expr_s { |_| [_, 10] }.sum to be >= 15 but got 12 for actual = 2'
+    end
+  end
+end
