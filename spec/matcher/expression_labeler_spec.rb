cba53052f1f1c5711e0c10f733988385492c4544
Rico Jasper <jasper.rico@gmail.com>
2024-11-25 18:46:59 +0100

    Test ExpressionLabeler

---
+++ spec/matcher/expression_labeler_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+describe Matcher::ExpressionLabeler do
+  let(:labeler) { Matcher::ExpressionLabeler.new }
+
+  it 'returns the same label for equivalent expressions' do
+    a = labeler.label(expression { (vars[:foo] + [1]).sum { |x| x % 2 } })
+    b = labeler.label(expression { (vars[:foo] + [1]).sum { |x| x % 2 } })
+    c = labeler.label(expression { (vars[:foo] + [2]).sum { |x| x % 2 } })
+    d = labeler.label(expression { (vars[:bar] + [1]).sum { |x| x % 2 } })
+    e = labeler.label(expression { (vars[:foo] - [1]).sum { |x| x % 2 } })
+    f = labeler.label(expression { (vars[:foo] + [1]).sum { |x| x % 10 } })
+
+    assert_equal a, b
+    refute_equal a, c
+    refute_equal a, d
+    refute_equal a, e
+    refute_equal a, f
+  end
+
+  it 'can substitute label for _' do
+    a = labeler.label(expression { _.foo })
+    b = labeler.label(expression { _.bar }, a)
+    c = labeler.label(expression { _.foo.bar })
+
+    assert_equal b, c
+
+    two_times = expression { _ + _ }
+    x = labeler.label(two_times)
+    y = labeler.label(two_times, x)
+    z = labeler.label(expression { (_ + _) + (_ + _) })
+
+    assert_equal y, z
+  end
+end
