e652f8f6866d0e8c658dc6d18cf71240007e76b2
Rico Jasper <jasper.rico@gmail.com>
2024-11-25 19:36:00 +0100

    Test NamespacedMessageBuilder

---
+++ spec/matcher/namespaced_message_builder_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+describe Matcher::NamespacedMessageBuilder do
+  it 'produces messages in a namespace' do
+    builder = Matcher::NamespacedMessageBuilder.new(false, 1, :my_realm)
+
+    message = builder.hello('world', from: 'aliens')
+
+    refute builder.hello.negated
+    assert_equal [:my_realm, :hello], message.key
+    assert_equal ['world'], message.args
+    assert_equal({ from: 'aliens' }, message.kwargs)
+  end
+
+  it '#not' do
+    builder = Matcher::NamespacedMessageBuilder.new(false, 1, :my_realm).not
+
+    assert_kind_of Matcher::NamespacedMessageBuilder, builder
+    assert builder.hello.negated
+  end
+end
