5d61e6e06ad204aa9fc17ea49d751613b591a0fa
Rico Jasper <jasper.rico@gmail.com>
2024-11-26 20:14:19 +0100

    Fix Collector specs

--- spec/matcher/errors/collector_spec.rb
+++ spec/matcher/errors/collector_spec.rb
@@ -2,24 +2,22 @@
 
 require 'test_helper'
 
-Collector = Matcher::Errors::Collector
-
 describe Matcher::Errors::Collector do
   include Matcher::ErrorsHelpers
 
+  let(:collector) { Matcher::Errors::Collector.new }
+
   it 'empty' do
-    assert_equal empty, Collector.new.node
+    assert_equal empty, collector.node
   end
 
   it 'add empty' do
-    collector = Collector.new
     collector << empty
 
     assert_equal empty, collector.node
   end
 
   it 'one' do
-    collector = Collector.new
     collector << element('foo')
 
     assert_equal element('foo'), collector.node
@@ -28,7 +26,6 @@ describe Matcher::Errors::Collector do
   it 'and + and' do
     a1, a2, b1, b2 = %w[a1 a2 b1 b2].map { element(_1) }
 
-    collector = Collector.new
     collector << _and(a1, a2)
     collector << _and(b1, b2)
 
@@ -36,29 +33,29 @@ describe Matcher::Errors::Collector do
   end
 
   it 'nested' do
-    collector = Collector.new
     collector[:foo] << element('foo')
 
-    assert_equal nested(:foo, element('foo')), collector.node
+    assert_equal nested(expression { _[:foo] }, element('foo')), collector.node
   end
 
   it 'deeply nested' do
-    collector = Collector.new
     collector[:foo][:bar] << element('foobar')
 
-    assert_equal nested(:foo, nested(:bar, element('foobar'))), collector.node
+    foo = expression { _[:foo] }
+    bar = expression { _[:bar] }
+
+    assert_equal nested(foo, nested(bar, element('foobar'))), collector.node
   end
 
   it 'nested via expression' do
-    collector = Collector.new
     collector[expression { _[:foo] }] << element('foobar')
 
-    assert_equal nested(:foo, element('foobar')), collector.node
+    foo = expression { _[:foo] }
+
+    assert_equal nested(foo, element('foobar')), collector.node
   end
 
   it '#<<: base error' do
-    collector = Collector.new
-
     assert_equal empty, collector.node
 
     collector << 'something went wrong'
@@ -73,26 +70,23 @@ describe Matcher::Errors::Collector do
   end
 
   it '#<<: field error' do
-    collector = Collector.new
-
     assert_equal empty, collector.node
 
     collector[:foo] << 'something went wrong'
     collector[:foo] << 'more errors'
 
     expected = nested(
-      :foo,
+      expression { _[:foo] },
       _and(
         element('something went wrong'),
         element('more errors'),
-      )
+      ),
     )
 
     assert_equal expected, collector.node
   end
 
   it '#<<: empty errors' do
-    collector = Collector.new
     collector << empty
 
     assert_equal empty, collector.node
