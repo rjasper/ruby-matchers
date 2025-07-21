57ac8f9c167cee83bb27d6a3b283669b5818afb0
Rico Jasper <jasper.rico@gmail.com>
2025-07-21 22:42:42 +0200

    Remove NegatedHashMatcher

--- lib/matcher/matchers/hash_matcher.rb
+++ lib/matcher/matchers/hash_matcher.rb
@@ -2,11 +2,13 @@
 
 module Matcher
   class HashMatcher < Base
-    def initialize(hash, partial: false)
+    def initialize(hash, partial: false, negated: false)
       super()
 
-      @hash = hash
+      @hash = negated ? hash.transform_values(&:~) : hash
+      @original_hash = hash
       @partial = partial
+      @negated = negated
       @includes_others = hash.include?(Others.instance)
       @includes_optionals = hash.each_key.any? { _1.is_a?(Optional) }
       @includes_expressions = hash.each_key.any? { _1.is_a?(Expression) }
@@ -21,10 +23,12 @@ module Matcher
     end
 
     def ~
-      NegatedHashMatcher.new(@hash, partial: @partial)
+      HashMatcher.new(@original_hash, partial: @partial, negated: !@negated)
     end
 
-    def check(actual)
+    def check(actual, &)
+      return negated_check(actual, &) if @negated
+
       unless actual.is_a?(Hash)
         errors << expected.kind_of(Hash)
         return
@@ -87,14 +91,92 @@ module Matcher
     protected :check
 
     def to_s
-      if @partial
-        "partial(#{@hash})"
+      if @negated
+        @partial ? "~partial(#{@original_hash})" : "neg(#{@original_hash})"
       else
-        @hash.to_s
+        @partial ? "partial(#{@hash})" : @hash.to_s
       end
     end
 
     private
+
+    def negated_check(actual)
+      return unless actual.is_a?(Hash)
+
+      if @hash.empty?
+        if @partial
+          errors << report.kind_of(Hash)
+        elsif actual.empty?
+          errors << report.predicate(:empty?)
+        end
+
+        return
+      end
+
+      if @includes_expressions
+        values_with_actual = values.merge(actual:)
+        expression_values = {}
+
+        @hash.each_key.with_index do |key, i|
+          key = key.value if key.is_a?(Optional)
+          expression_values[i] = key.evaluate(values_with_actual) if key.is_a?(Expression)
+        end
+      end
+
+      expected_keys = if @includes_optionals || @includes_expressions
+        @hash.keys.map.with_index do |k, i|
+          k = k.value if k.is_a?(Optional)
+          k = expression_values[i] if k.is_a?(Expression)
+          k
+        end
+      else
+        @hash.keys
+      end
+
+      extra_keys = actual.keys - expected_keys
+
+      return if !@partial && !@includes_others && !extra_keys.empty?
+
+      collector = new_collector.or!
+
+      @hash.each_with_index do |(key, value), i|
+        if key.is_a?(Others)
+          result = yield(others, actual.slice(*extra_keys))
+
+          return if result.valid?
+
+          collector << result
+
+          next
+        end
+
+        is_optional = key.is_a?(Optional)
+        key = key.value if is_optional
+        error_key = key
+        key = expression_values[i] if key.is_a?(Expression)
+        actual_value = actual[key]
+
+        if actual_value.nil? && !actual.key?(key)
+          next if is_optional
+
+          return
+        end
+
+        result = yield(value, actual_value, key:, parent: actual)
+
+        return if result.valid?
+
+        error_key = key_call_for(error_key) if error_key.is_a?(Expression)
+
+        collector[error_key] << result
+      end
+
+      errors << if collector.empty?
+        report.predicate(:empty?)
+      else
+        collector.error
+      end
+    end
 
     def key_call_for(key)
       Call.new(Variable.actual, :[], [key])
