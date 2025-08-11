623a5c8c897fe2e24c7f5557f0572a237ab783db
Rico Jasper <jasper.rico@gmail.com>
2025-08-11 23:24:03 +0200

    Add ParseJsonMatcher

---
+++ lib/matcher/matchers/parse_json_matcher.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+module Matcher
+  class ParseJsonMatcher < Base
+    extend OnceBefore
+
+    def initialize(matcher, json_options: nil, negated: false)
+      super()
+
+      @matcher = negated ? ~matcher : matcher
+      @original_matcher = matcher
+      @json_options = json_options
+      @negated = negated
+    end
+
+    once_before :initialize do
+      require 'json'
+    end
+
+    def ~
+      ParseJsonMatcher.new(@original_matcher, json_options: @json_options, negated: !@negated)
+    end
+
+    def check(state)
+      actual = state.actual
+
+      unless actual.is_a?(String)
+        state.errors << expected.kind_of(String) unless @negated
+        return
+      end
+
+      value = JSON.parse(actual, **@json_options)
+
+      if @matcher.is_a?(NeverMatcher)
+        state.errors << expected.not.valid_format(:json)
+        return
+      end
+
+      result = yield(@matcher, value)
+
+      return if result.valid?
+
+      parse_json = Call.new(Constant.new(JSON), :parse, [Variable.actual])
+      state.errors[parse_json] << result
+    rescue JSON::ParserError
+      state.errors << expected.valid_format(:json) unless @negated
+    end
+
+    def to_s
+      prefix = @negated ? '~' : ''
+
+      return "#{prefix}json_format" if @original_matcher.is_a?(AlwaysMatcher)
+
+      "#{prefix}parse_json(#{@original_matcher})"
+    end
+  end
+
+  module MatcherBuilding
+    def parse_json(matcher = UNDEFINED, **)
+      return Pipe.new { parse_json(_1, **) } if Matcher.undefined?(matcher)
+
+      matcher = Matcher.of(matcher)
+      json_options = {}.merge(**)
+      json_options = nil if json_options.empty?
+
+      ParseJsonMatcher.new(matcher, json_options:)
+    end
+
+    def json_format
+      @json_format ||= ParseJsonMatcher.new(AlwaysMatcher.instance)
+    end
+  end
+end
---
+++ spec/matcher/matchers/parse_json_matcher_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+describe Matcher::ParseJsonMatcher do
+  let(:json_parse) do
+    expression { expr(JSON).parse(_) }
+  end
+
+  it 'is built by parse_json' do
+    klass = Matcher::ParseJsonMatcher
+
+    assert_kind_of(klass, Matcher.build { parse_json(Hash) })
+    assert_kind_of(klass, Matcher.build { parse_json ^ Hash })
+    assert_kind_of(klass, Matcher.build { json_format })
+  end
+
+  it 'matches JSON' do
+    matcher = Matcher.build { parse_json(Hash) }
+    negated = ~matcher
+
+    assert_no_errors matcher.match('{"a": 1}')
+    assert_errors negated.match('{"a": 1}'),
+      json_parse => msg({ 'a' => 1 }).kind_of(Hash)
+
+    assert_errors matcher.match(nil),
+      msg(nil).not.kind_of(String)
+    assert_no_errors negated.match(nil)
+
+    assert_errors matcher.match(''),
+      msg('').not.valid_format(:json)
+    assert_no_errors negated.match('')
+
+    assert_errors matcher.match('1'),
+      json_parse => msg(1).not.kind_of(Hash)
+    assert_no_errors negated.match('1')
+  end
+
+  it 'validates JSON format' do
+    matcher = Matcher.build { json_format }
+    negated = ~matcher
+
+    assert_no_errors matcher.match('{"a": 1}')
+    assert_errors negated.match('{"a": 1}'),
+      msg('{"a": 1}').valid_format(:json)
+
+    assert_errors matcher.match(nil),
+      msg(nil).not.kind_of(String)
+    assert_no_errors negated.match(nil)
+
+    assert_errors matcher.match(''),
+      msg('').not.valid_format(:json)
+    assert_no_errors negated.match('')
+  end
+
+  it 'passes options to JSON.parse' do
+    matcher = Matcher.build do
+      parse_json(symbolize_names: true) ^ { foo: 'bar' }
+    end
+
+    assert_errors matcher.match('{"foo": "qux"}'),
+      json_parse => { foo: msg('qux').not.equal('bar') }
+  end
+
+  it '#to_s' do
+    assert_equal 'parse_json(Hash)', Matcher.build { parse_json(Hash) }.to_s
+    assert_equal '~parse_json(Hash)', Matcher.build { ~parse_json(Hash) }.to_s
+    assert_equal 'json_format', Matcher.build { json_format }.to_s
+    assert_equal '~json_format', Matcher.build { ~json_format }.to_s
+  end
+end
