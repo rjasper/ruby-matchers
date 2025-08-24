4de77e14e2b9e05a72f51f71de8cb28e4dc911d9
Rico Jasper <jasper.rico@gmail.com>
2025-08-24 14:06:40 +0200

    Remove message binding

--- lib/matcher/messages/message.rb
+++ lib/matcher/messages/message.rb
@@ -12,16 +12,8 @@ module Matcher
       @kwargs = kwargs
     end
 
-    def bound?
-      !Matcher.undefined?(@actual)
-    end
-
     def negate!
       @negated = !@negated
-    end
-
-    def bind(actual)
-      Message.new(@key, @negated, actual, *@args, **@kwargs)
     end
 
     def ==(other)
