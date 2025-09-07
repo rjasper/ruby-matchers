0db1778f86c9dacc0f91b6f9312bf5637ccb8bd1
Rico Jasper <jasper.rico@gmail.com>
2025-09-07 17:21:03 +0200

    Add MatchData matcher to RegexpMatcher

--- lib/matcher/matchers/regexp_matcher.rb
+++ lib/matcher/matchers/regexp_matcher.rb
@@ -8,15 +8,17 @@ module Matcher
       (matcher_cache.regexp_matchers ||= {})[pattern] ||= new(pattern)
     end
 
-    def initialize(pattern, negated: false)
+    def initialize(pattern, matcher = AlwaysMatcher.instance, negated: false)
       super()
 
       @pattern = pattern
+      @matcher = negated ? ~matcher : matcher
+      @original_matcher = matcher
       @negated = negated
     end
 
     def negate
-      RegexpMatcher.new(@pattern, negated: !@negated)
+      RegexpMatcher.new(@pattern, @original_matcher, negated: !@negated)
     end
 
     def validate(state)
@@ -25,16 +27,44 @@ module Matcher
         return
       end
 
-      state.errors << state.expected.not_if(@negated).matching(@pattern) if
-        @pattern.match?(state.actual) == @negated
+      if @original_matcher == AlwaysMatcher.instance
+        state.errors << state.expected.not_if(@negated).matching(@pattern) if
+          @pattern.match?(state.actual) == @negated
+      else
+        match = @pattern.match(state.actual)
+
+        if match
+          state.errors[match_pattern_call] << yield(@matcher, match)
+        else
+          state.errors << state.expected.matching(@pattern) unless @negated
+        end
+      end
+    end
+
+    def match_pattern_call
+      @match_pattern_call ||=
+        Call.new(Variable.actual, :match, [Constant.new(@pattern)])
     end
 
     def to_s
-      if @negated
-        "neg(#{@pattern.inspect})"
+      if @original_matcher == AlwaysMatcher.instance
+        if @negated
+          "neg(#{@pattern.inspect})"
+        else
+          @pattern.inspect
+        end
       else
-        @pattern.inspect
+        "#{'~' if @negated}regexp(#{@pattern.inspect}, #{@original_matcher})"
       end
+    end
+  end
+
+  module MatcherBuilding
+    def regexp(pattern, matcher = UNDEFINED)
+      return Pipe.new { regexp(pattern, _1) }.optional if
+        Matcher.undefined?(matcher)
+
+      RegexpMatcher.new(pattern, matcher_of(matcher))
     end
   end
 end
--- spec/matcher/matchers/regexp_matcher_spec.rb
+++ spec/matcher/matchers/regexp_matcher_spec.rb
@@ -7,6 +7,17 @@ describe Matcher::RegexpMatcher do
     assert_kind_of(Matcher::RegexpMatcher, Matcher.build { /f/ })
   end
 
+  it 'is built by regexp' do
+    kind = Matcher::RegexpMatcher
+
+    assert_kind_of(kind, Matcher.build { regexp(/foo/) })
+    assert_kind_of(kind, Matcher.build { ~regexp(/foo/) })
+    assert_kind_of(kind, Matcher.build { regexp(/foo/, _) })
+    assert_kind_of(kind, Matcher.build { ~regexp(/foo/, _) })
+    assert_kind_of(kind, Matcher.build { regexp(/foo/) ^ _ })
+    assert_kind_of(kind, Matcher.build { ~regexp(/foo/) ^ _ })
+  end
+
   it 'matches Regexp' do
     matcher = Matcher.build { /f/ }
     negated = ~matcher
@@ -24,11 +35,52 @@ describe Matcher::RegexpMatcher do
     assert_no_errors negated.match('bar')
   end
 
+  it 'matches MatchData' do
+    matcher = Matcher.build do
+      regexp(/x=(\d+)/) ^ project(_[1].to_i) ^ (_ > 10)
+    end
+
+    negated = ~matcher
+    key = expression { _.match(/x=(\d+)/)[1].to_i }
+
+    assert matcher.match?('x=20')
+    assert_no_errors matcher.match('x=20')
+    refute negated.match?('x=20')
+    assert_errors negated.match('x=20'),
+      key => msg(20).greater_than(10)
+
+    refute matcher.match?('x=5')
+    assert_errors matcher.match('x=5'),
+      key => msg(5).not.greater_than(10)
+    assert negated.match?('x=5')
+    assert_no_errors negated.match('x=5')
+
+    refute matcher.match?('y=5')
+    assert_errors matcher.match('y=5'),
+      msg('y=5').not.matching(/x=(\d+)/)
+    assert negated.match?('y=5')
+    assert_no_errors negated.match('y=5')
+
+    refute matcher.match?(5)
+    assert_errors matcher.match(5),
+      msg(5).not.kind_of(String)
+    assert negated.match?(5)
+    assert_no_errors negated.match(5)
+  end
 
   it '#to_s' do
     matcher = Matcher::RegexpMatcher.new(/asdf/)
 
     assert_equal '/asdf/', matcher.to_s
     assert_equal 'neg(/asdf/)', matcher.~.to_s
+  end
+
+  it '#to_s: with matcher' do
+    matcher = Matcher.build do
+      regexp(/x=(\d+)/) ^ project(_[1].to_i) ^ (_ > 10)
+    end
+
+    assert_equal 'regexp(/x=(\\d+)/, project(_[1].to_i => _ > 10))', matcher.to_s
+    assert_equal '~regexp(/x=(\\d+)/, project(_[1].to_i => _ > 10))', matcher.~.to_s
   end
 end
