c464a2a2deef9c63bf2bb340952cab3f2368acfc
Rico Jasper <jasper.rico@gmail.com>
2025-11-09 20:28:05 +0100

    Add #to_standard to Message

--- lib/matcher/messages/message.rb
+++ lib/matcher/messages/message.rb
@@ -32,6 +32,52 @@ module Matcher
       @hash ||= [self.class, @key, @negated, @actual, @args, @kwargs].hash
     end
 
+    def namespace
+      @key.is_a?(Array) ? @key[0] : nil
+    end
+
+    def to_standard
+      return self if namespace != :expression
+
+      key = @key[1]
+
+      case key
+      when :truthy
+        _expr, value = @args
+        Message.new(key, @negated, value)
+      when :same
+        _l_expr, _r_expr, left, right = @args
+        Message.new(:same, @negated, left, right)
+      when :comparable_to, :having_key, :in, :including, :matching, :instance_of, :kind_of, :responding_to, :predicate
+        _expr, value, operand = @args
+        Message.new(key, @negated, value, operand)
+      when :between, :length_of
+        _expr, value, operand1, operand2 = @args
+        Message.new(key, @negated, value, operand1, operand2)
+      when :comparison
+        expr, left, right = @args
+
+        case expr.method
+        when :==
+          Message.new(:equal, @negated, left, right)
+        when :!=
+          Message.new(:equal, !@negated, left, right)
+        when :<
+          Message.new(:less_than, @negated, left, right)
+        when :>
+          Message.new(:greater_than, @negated, left, right)
+        when :<=
+          Message.new(:less_than_or_equal, @negated, left, right)
+        when :>=
+          Message.new(:greater_than_or_equal, @negated, left, right)
+        else
+          raise "unexpected method: #{expr.method.inspect}"
+        end
+      else
+        self
+      end
+    end
+
     def to_s
       if @key.is_a?(Array)
         namespace, key = @key
--- spec/matcher/messages/message_spec.rb
+++ spec/matcher/messages/message_spec.rb
@@ -23,6 +23,179 @@ module Matcher
         assert_equal 'report(1).hello("world", tone: "friendly")',
           msg(1).hello('world', tone: 'friendly').to_s
       end
+
+      describe '#to_standard' do
+        it 'truthy' do
+          matcher = Matcher.build { _[0] }
+          message = matcher.match([false]).message
+          expected = Message.new(:truthy, true, false)
+
+          assert_equal :expression, message.namespace
+          assert_equal expected, message.to_standard
+        end
+
+        it 'same' do
+          matcher = Matcher.build { _[0].equal?(vars[:a]) }
+          message = matcher.match([2], a: 1).message
+          expected = Message.new(:same, true, 2, 1)
+
+          assert_equal :expression, message.namespace
+          assert_equal expected, message.to_standard
+        end
+
+        it 'comparable_to' do
+          matcher = Matcher.build { _[0] <=> vars[:a] }
+          message = matcher.match(['hi'], a: 1).message
+          expected = Message.new(:comparable_to, true, 'hi', 1)
+
+          assert_equal :expression, message.namespace
+          assert_equal expected, message.to_standard
+        end
+
+        it 'having_key' do
+          matcher = Matcher.build { _[0].key?(vars[:a]) }
+          message = matcher.match([{}], a: 1).message
+          expected = Message.new(:having_key, true, {}, 1)
+
+          assert_equal :expression, message.namespace
+          assert_equal expected, message.to_standard
+        end
+
+        it 'in' do
+          matcher = Matcher.build { vars[:collection].include?(_.upcase)}
+          message = matcher.match('c', collection: %w[A B]).message
+          expected = Message.new(:in, true, 'C', %w[A B])
+
+          assert_equal :expression, message.namespace
+          assert_equal expected, message.to_standard
+        end
+
+        it 'including' do
+          matcher = Matcher.build { (_ + [3]).include?(vars[:a]) }
+          message = matcher.match([1, 2], a: 4).message
+          expected = Message.new(:including, true, [1, 2, 3], 4)
+
+          assert_equal :expression, message.namespace
+          assert_equal expected, message.to_standard
+        end
+
+        it 'matching' do
+          matcher = Matcher.build { concat(_, '!') =~ vars[:a] }
+          message = matcher.match('Hi', a: /Hello/).message
+          expected = Message.new(:matching, true, 'Hi!', /Hello/)
+
+          assert_equal :expression, message.namespace
+          assert_equal expected, message.to_standard
+        end
+
+        it 'instance_of' do
+          matcher = Matcher.build { _[0].instance_of?(vars[:a]) }
+          message = matcher.match([1], a: String).message
+          expected = Message.new(:instance_of, true, 1, String)
+
+          assert_equal :expression, message.namespace
+          assert_equal expected, message.to_standard
+        end
+
+        it 'kind_of' do
+          matcher = Matcher.build { _[0].is_a?(vars[:a]) }
+          message = matcher.match([1], a: String).message
+          expected = Message.new(:kind_of, true, 1, String)
+
+          assert_equal :expression, message.namespace
+          assert_equal expected, message.to_standard
+        end
+
+        it 'responding_to' do
+          matcher = Matcher.build { _[0].respond_to?(vars[:a]) }
+          message = matcher.match([1], a: :some_method).message
+          expected = Message.new(:responding_to, true, 1, :some_method)
+
+          assert_equal :expression, message.namespace
+          assert_equal expected, message.to_standard
+        end
+
+        it 'predicate' do
+          matcher = Matcher.build { _[0].even? }
+          message = matcher.match([1]).message
+          expected = Message.new(:predicate, true, 1, :even?)
+
+          assert_equal :expression, message.namespace
+          assert_equal expected, message.to_standard
+        end
+
+        it 'between' do
+          matcher = Matcher.build { _[0].between?(vars[:min], vars[:max]) }
+          message = matcher.match([-1], min: 0, max: 10).message
+          expected = Message.new(:between, true, -1, 0, 10)
+
+          assert_equal :expression, message.namespace
+          assert_equal expected, message.to_standard
+        end
+
+        it 'length_of' do
+          matcher = Matcher.build { (_ + [3]).length == vars[:a] }
+          message = matcher.match([1, 2], a: 2).message
+          expected = Message.new(:length_of, true, [1, 2, 3], 2, 3)
+
+          assert_equal :expression, message.namespace
+          assert_equal expected, message.to_standard
+        end
+
+        it '==' do
+          matcher = Matcher.build { _ * 2 == vars[:a] }
+          message = matcher.match(2, a: 6).message
+          expected = Message.new(:equal, true, 4, 6)
+
+          assert_equal :expression, message.namespace
+          assert_equal expected, message.to_standard
+        end
+
+        it '!=' do
+          matcher = Matcher.build { _ * 2 != vars[:a] }
+          message = matcher.match(2, a: 4).message
+          expected = Message.new(:equal, false, 4, 4)
+
+          assert_equal :expression, message.namespace
+          assert_equal expected, message.to_standard
+        end
+
+        it '<' do
+          matcher = Matcher.build { _ * 2 < vars[:a] }
+          message = matcher.match(4, a: 6).message
+          expected = Message.new(:less_than, true, 8, 6)
+
+          assert_equal :expression, message.namespace
+          assert_equal expected, message.to_standard
+        end
+
+        it '>' do
+          matcher = Matcher.build { _ * 2 > vars[:a] }
+          message = matcher.match(2, a: 6).message
+          expected = Message.new(:greater_than, true, 4, 6)
+
+          assert_equal :expression, message.namespace
+          assert_equal expected, message.to_standard
+        end
+
+        it '<=' do
+          matcher = Matcher.build { _ * 2 <= vars[:a] }
+          message = matcher.match(4, a: 6).message
+          expected = Message.new(:less_than_or_equal, true, 8, 6)
+
+          assert_equal :expression, message.namespace
+          assert_equal expected, message.to_standard
+        end
+
+        it '>=' do
+          matcher = Matcher.build { _ * 2 >= vars[:a] }
+          message = matcher.match(2, a: 6).message
+          expected = Message.new(:greater_than_or_equal, true, 4, 6)
+
+          assert_equal :expression, message.namespace
+          assert_equal expected, message.to_standard
+        end
+      end
     end
   end
 end
