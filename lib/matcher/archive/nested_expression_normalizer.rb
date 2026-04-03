942e7ac2170784827ebed824e8390ffd4f647fd1
Rico Jasper <jasper.rico@gmail.com>
2026-04-03 12:11:57 +0200

    Fix shadowing

--- lib/matcher/archive/nested_expression_normalizer.rb
+++ lib/matcher/archive/nested_expression_normalizer.rb
@@ -181,8 +181,8 @@ module Matcher
 
       while cur.is_a?(Call)
         if cur.method == :[] && cur.binary?
-          e = !trace.empty? && trace.reduce(Variable.actual) do |expr, cur|
-            Call.new(expr, cur.method, cur.args, cur.kwargs, cur.block)
+          e = !trace.empty? && trace.reduce(Variable.actual) do |expr, call|
+            Call.new(expr, call.method, call.args, call.kwargs, call.block)
           end
 
           tail <<= e if e
