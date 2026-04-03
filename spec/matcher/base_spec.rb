26c93e11704d2c4fc5bfcc78c67106af97a6e797
Rico Jasper <jasper.rico@gmail.com>
2026-04-03 19:38:50 +0200

    Refactor stubbing max_reference_depth

--- spec/matcher/base_spec.rb
+++ spec/matcher/base_spec.rb
@@ -3,15 +3,22 @@
 require "test_helper"
 
 describe Matcher::Base do
+  def setup
+    @max_reference_depth_bak = Matcher.max_reference_depth
+    Matcher.max_reference_depth = 2
+  end
+
+  def teardown
+    Matcher.max_reference_depth = @max_reference_depth_bak
+  end
+
   it "checks match level" do
     matcher = Matcher.build do
       refs[:obj] = project(_.dup) ^ refs[:obj]
     end
 
-    Matcher.stub(:max_reference_depth, 2) do
-      assert_errors matcher.match(Object.new),
-        expression { _.dup.dup.dup } => "match level too deep: 3"
-    end
+    assert_errors matcher.match(Object.new),
+      expression { _.dup.dup.dup } => "match level too deep: 3"
   end
 
   it "scopes values" do
